04fed61951a3e34ab63edaa81b59f672bd914066
macbook <Win95mustdie>
2018-01-11 15:43:05 +0300

    first classes

---
+++ main/java/Dot.java
@@ -0,0 +1,8 @@
+public class Dot  {
+    int coordinateX, coordinateY;
+    public Dot (int x, int y){
+        coordinateX=x;
+        coordinateY=y;
+    }
+
+}
---
+++ main/java/Triangle.java
@@ -0,0 +1,34 @@
+public class Triangle {
+    //    int X1,X2,X3,Y1,Y2,Y3;
+//    Dot first =new Dot(X1,Y1);
+//    Dot second =new Dot(X2,Y2);
+//    Dot third =new Dot(X3,Y3);
+    public static void main(String[] args) {
+
+
+    }
+
+    double x1 = 1;
+    double x2 = 2;
+    double x3 = 3;
+    double y1 = 4;
+    double y2 = 5;
+    double y3 = 6;
+    double perimetr;
+    double square;
+
+    public double Perimetr() {
+        perimetr = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) + Math.sqrt(Math.pow((x3 - x2), 2) + Math.pow((y3 - y2), 2)) +
+                Math.sqrt(Math.pow((x3 - x1), 2) + Math.pow((y3 - y1), 2));
+        System.out.println("периметр = " + perimetr);
+        return perimetr;
+    }
+
+    public double Square() {
+        square = 1 / 2 * Math.abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3));
+        System.out.println("площадь = " + square);
+
+        return square;
+    }
+
+}
